28f99b5c92ef2e934ca4bc294e4f5c962c1a83c3
sandeshprasai <sandeshprasai4321@gmail.com>
2025-06-10 08:01:27 +0545

    POST Method Implemented

--- app/src/main/java/com/mobile/bce/ProductActivity.kt
+++ app/src/main/java/com/mobile/bce/ProductActivity.kt
@@ -1,6 +1,7 @@
 package com.mobile.bce
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mobile.bce.adapter.ProductAdapter
+import com.mobile.bce.model.Rating
 import com.mobile.bce.model.product
 import com.mobile.bce.network.RetrofitClient
 import retrofit2.Call
@@ -21,8 +23,43 @@ class ProductActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product)
+        val postButton = findViewById<Button>(R.id.addItems)
         recyclerView = findViewById(R.id.productRecyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        postButton.setOnClickListener {
+
+            val sampleProduct = product(
+                id = 21,
+                title = "Static Product",
+                price = 99.99,
+                description= "This is simple static product",
+                category = "electronics",
+                image= "https://i.pravatar.cc",
+                rating =Rating(
+                    rate= 4.5,
+                    count = 200
+                )
+            )
+
+            val call = RetrofitClient.api.postProduct(sampleProduct)
+            call.enqueue(object : Callback<product>{
+                override fun onResponse(call: Call<product>, response: Response<product>) {
+                    if (response.isSuccessful) {
+                        Toast.makeText(this@ProductActivity, "Product Posted Successfully", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(this@ProductActivity, "Failed: ${response.code()}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<product>, t: Throwable) {
+                    Toast.makeText(this@ProductActivity, "Error: ${t.localizedMessage}", Toast.LENGTH_LONG).show()
+                }
+            })
+        }
+
+
+
         fetchProducts()
     }
 
--- app/src/main/java/com/mobile/bce/network/ApiInterface.kt
+++ app/src/main/java/com/mobile/bce/network/ApiInterface.kt
@@ -2,10 +2,14 @@ package com.mobile.bce.network
 
 import com.mobile.bce.model.product
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiInterface {
     @GET("products")
     fun getProducts(): Call<List<product>>
 
+    @POST("products")
+    fun postProduct(@Body product: product): Call<product>
 }
